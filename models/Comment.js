ce5d50ed1d5bbe2c0b35695030658f98753cd387
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-12-12 12:57:32 -0600

    added models logic and server.js logic

---
+++ models/Comment.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoincrement: true
+        }
+    },
+    {
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    },
+    {
+        //this is a foreign key, id who creates post
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        //this is a foreign key, id who creates post
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Comment;
